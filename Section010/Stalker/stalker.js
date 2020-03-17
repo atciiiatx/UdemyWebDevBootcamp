41e915d2b9c6ff1b0170a5ea263ab8a3182199e9
AT Campbell <at.campbell@clearcorrect.com>
2020-03-17 11:28:00 -0500

    Section 10

---
+++ Section010/Stalker/stalker.js
@@ -0,0 +1,5 @@
+first = prompt("What is your first name?");
+last = prompt("What is your last nasme?");
+age = prompt("What is your age?");
+console.log("Nice to meet you, " + first + " "+ last);
+console.log("You are " + age + " years old");
